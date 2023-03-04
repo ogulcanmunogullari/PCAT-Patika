ed860f0c0c27562441cb74d729bd209943414e91
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2023-03-04 21:08:16 +0300

    add photo and photo page

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from "express"
 import Photo from "./models/Photo.js"
 import mongoose from "mongoose"
-
+import path from "path"
+import fileUpload from "express-fileupload"
+import fs from "fs"
 const app = express()
+const __dirname = path.resolve()
 
 //connect to mongodb
-mongoose.connect("mongodb://localhost/pcat-test-db")
+mongoose.connect(
+  "mongodb+srv://ogulcan:12qwaszx@database.xjxvdni.mongodb.net/pcatdb?retryWrites=true&w=majority"
+)
 
 //Template engine
 app.set("view engine", "ejs")
@@ -14,8 +19,10 @@ app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(fileUpload())
 
 //Routes
+//Get
 app.get("/", async (req, res) => {
   const photos = await Photo.find({})
   res.render("index", { photos })
@@ -26,9 +33,30 @@ app.get("/about", (req, res) => {
 app.get("/add", (req, res) => {
   res.render("add")
 })
+app.get("/photos/:id", async (req, res) => {
+  const photo = await Photo.findById(req.params.id)
+  res.render("photo", {
+    photo,
+  })
+})
+
+//Post
 app.post("/photos", async (req, res) => {
-  await Photo.create(req.body)
-  res.redirect("/")
+  const uploadDir = "public/uploads"
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir)
+  }
+
+  let uploadeImage = req.files.image
+  let uploadPath = __dirname + "/public/uploads/" + uploadeImage.name
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: "/uploads/" + uploadeImage.name,
+    })
+    res.redirect("/")
+  })
 })
 
 const port = 3000
